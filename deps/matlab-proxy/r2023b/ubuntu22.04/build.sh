4d84e22caf620fd2e8a46fdbeaa9a07d9f7c70cc
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-02-26 14:51:25 +0200

    push container

--- deps/matlab-proxy/r2023b/ubuntu22.04/build.sh
+++ deps/matlab-proxy/r2023b/ubuntu22.04/build.sh
@@ -6,3 +6,10 @@ cd "${0%/*}"
 
 # Build the container
 sudo docker build --tag "ghcr.io/cscfi/matlab-proxy:r2023b-ubuntu22.04" .
+
+# Login to GitHub container registry
+# Prompts for an access token
+sudo docker login -u cscfi ghcr.io
+
+# Push the container
+sudo docker push "ghcr.io/cscfi/matlab-proxy:r2023b-ubuntu22.04"
