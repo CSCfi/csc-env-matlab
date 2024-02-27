bd9736f6ed21d388aab941196e530957e5ca214c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-02-27 14:11:25 +0200

    simpler flags

--- proxy/r2023b/matlab.lua
+++ proxy/r2023b/matlab.lua
@@ -13,7 +13,6 @@ prepend_path("PATH", bin_dir)
 -- Path to the matlab container image run using `apptainer_wrapper`.
 image = pathJoin(appl_dir, "proxy", version, "matlab.sif")
 setenv("SING_IMAGE", image)
-
--- Flags
-flags = "--bind=/usr/lib64/libmunge.so.2:/lib/x86_64-linux-gnu/libmunge.so.2,/var/run/munge --bind /run"
+--flags = "--bind /run --bind=/usr/lib64/libmunge.so.2:/lib/x86_64-linux-gnu/libmunge.so.2,/var/run/munge"
+flags = "--bind /run"
 setenv("SING_FLAGS", flags)
