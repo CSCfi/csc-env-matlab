fd6a9134ee4e0a6363e1a2d0d36ab0c31fba749f
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-02-27 13:21:53 +0200

    environment

--- mps/r2023b/matlab.lua
+++ mps/r2023b/matlab.lua
@@ -10,11 +10,6 @@ appl_dir = "/appl/soft/math/matlab"
 bin_dir = pathJoin(appl_dir, "mps", version, "bin")
 prepend_path("PATH", bin_dir)
 
--- Add matlab path to environment.
--- It is the path where matlab is mounted inside to the container.
-matlab = "/opt/matlab"
-setenv("MATLAB", matlab)
-
 -- Path to the matlab container run with `apptainer_wrapper`.
 image = pathJoin(appl_dir, "mps", version, "matlab.sif")
 setenv("SING_IMAGE", image)
