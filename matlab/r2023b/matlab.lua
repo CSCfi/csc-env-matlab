cfa1351676e981bb0f4e80996d168cc1b39a36ee
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-02-27 13:14:14 +0200

    set path to matlab binaries in container

--- matlab/r2023b/matlab.lua
+++ matlab/r2023b/matlab.lua
@@ -10,14 +10,10 @@ appl_dir = "/appl/soft/math/matlab"
 bin_dir = pathJoin(appl_dir, "interactive", version, "bin")
 prepend_path("PATH", bin_dir)
 
--- Add matlab path to environment.
--- It is the path where matlab is mounted inside to the container.
-matlab = "/opt/matlab"
-setenv("MATLAB", matlab)
-
 -- Path to the matlab container image run using `apptainer_wrapper`.
 image = pathJoin(appl_dir, "interactive", version, "matlab.sif")
 setenv("SING_IMAGE", image)
+setenv("SING_FLAGS", "-B /run")
 
 -- Load message and license disclaimer
 loadmsg = "MATLAB\nVersion: " .. version .. "\nLicense: Academic\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n----------"
