48fb7cafbb1d523917459d85ede9f02b4b272e16
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-02-27 13:23:37 +0200

    environment

--- proxy/r2023b/matlab.lua
+++ proxy/r2023b/matlab.lua
@@ -10,16 +10,6 @@ appl_dir = "/appl/soft/math/matlab"
 bin_dir = pathJoin(appl_dir, "proxy", version, "bin")
 prepend_path("PATH", bin_dir)
 
--- Add matlab path to environment.
--- It is the path where matlab is mounted inside to the container.
-matlab = "/opt/matlab"
-setenv("MATLAB", matlab)
---prepend_path("MATLABPATH", matlab)
-
--- Add binaries inside the MATLAB container to path
-matlab_bin = "/opt/matlab/bin"
-prepend_path("PATH", matlab_bin)
-
 -- Path to the matlab container image run using `apptainer_wrapper`.
 image = pathJoin(appl_dir, "proxy", version, "matlab.sif")
 setenv("SING_IMAGE", image)
