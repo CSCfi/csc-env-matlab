88115d9093d7f67a16d86784e70c8f108c46c2ee
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-03-07 14:51:53 +0200

    replace docker with podman to produce oci containers

--- deps/matlab-proxy/r2023b/ubuntu22.04/build.sh
+++ deps/matlab-proxy/r2023b/ubuntu22.04/build.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 cd "${0%/*}"
 
 # Build the container
-sudo docker build --tag "ghcr.io/cscfi/matlab-proxy:r2023b-ubuntu22.04" .
+sudo podman build --tag "ghcr.io/cscfi/matlab-proxy:r2023b-ubuntu22.04" .
 
 # Login to GitHub container registry
 # Prompts for an access token
-sudo docker login -u cscfi ghcr.io
+sudo podman login -u cscfi ghcr.io
 
 # Push the container
-sudo docker push "ghcr.io/cscfi/matlab-proxy:r2023b-ubuntu22.04"
+sudo podman push "ghcr.io/cscfi/matlab-proxy:r2023b-ubuntu22.04"
