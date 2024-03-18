8b2ff9554e803e6c954a08f44772103ddfbd6e67
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-03-18 10:33:04 +0200

    replace apptainer wrapper

--- mps/r2023b/matlab.lua
+++ mps/r2023b/matlab.lua
@@ -12,7 +12,7 @@ prepend_path("PATH", bin_dir)
 
 -- Path to the matlab container run with `apptainer_wrapper`.
 image = pathJoin(appl_dir, "mps", version, "matlab.sif")
-setenv("SING_IMAGE", image)
+setenv("MATLAB_SING_IMAGE", image)
 
 -- Load message and license disclaimer
 loadmsg = "MATLAB Parallel Server\nVersion: " .. version .. "\nLicense: Academic\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n----------\n\nMATLAB Parallel Server (MPS) is not intended to be used as a batch job. This module is for debugging purposes. Read more about how to use MPS from the documentation at:\nhttps://docs.csc.fi/apps/matlab/"
