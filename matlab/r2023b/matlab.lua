3de0be772ea470fcefeff4bf2aa12f0bd8651a7e
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-03-18 09:26:14 +0200

    use explicit appatiner_exec wrapper

--- matlab/r2023b/matlab.lua
+++ matlab/r2023b/matlab.lua
@@ -10,10 +10,9 @@ appl_dir = "/appl/soft/math/matlab"
 bin_dir = pathJoin(appl_dir, "interactive", version, "bin")
 prepend_path("PATH", bin_dir)
 
--- Path to the matlab container image run using `apptainer_wrapper`.
+-- Path to the matlab container image to run using apptainer or singularity.
 image = pathJoin(appl_dir, "interactive", version, "matlab.sif")
 setenv("SING_IMAGE", image)
-setenv("SING_FLAGS", "-B /run")
 
 -- Load message and license disclaimer
 loadmsg = "MATLAB\nVersion: " .. version .. "\nLicense: Academic\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n----------"
