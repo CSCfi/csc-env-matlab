c649997621d647072fa7eaeaa8a4e832d56faab9
Henrik Nortamo <40563680+Nortamo@users.noreply.github.com>
2024-03-20 13:17:22 +0200

    Update matlab.lua
    Adds version and license information + printed disclaimer

--- lumi/r2023b/matlab.lua
+++ lumi/r2023b/matlab.lua
@@ -13,3 +13,10 @@ prepend_path("PATH", bin_dir)
 -- Path to the matlab container image run using `apptainer_wrapper`.
 image = pathJoin(appl_dir, "interactive", version, "matlab.sif")
 setenv("MATLAB_SING_IMAGE", image)
+
+-- Load message and license disclaimer
+loadmsg = "MATLAB\nVersion: " .. version .. "\nLicense: Academic\n----------\nThe terms of use of this software allow its use for only in teaching and academic research at a degree-granting institute. If you are unsure whether you are allowed to use the software, please unload this module now.\n\nIf you want to use your own license with the installation, please contact LUMI support for help.\n----------"
+
+if (mode() == "load") then
+  LmodMessage(loadmsg)
+end
