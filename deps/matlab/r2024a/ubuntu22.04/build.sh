11a8ecdbde233b09f3d2b37cdf8a07544d04d898
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-04-05 12:45:27 +0300

    build script

---
+++ deps/matlab/r2024a/ubuntu22.04/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# directory of the script
+cd "${0%/*}"
+
+# Build the container
+sudo podman build --tag "ghcr.io/cscfi/matlab:r2024a-ubuntu22.04" .
+
+# Login to GitHub container registry
+# Prompts for an access token
+sudo podman login -u cscfi ghcr.io
+
+# Push the container
+sudo podman push "ghcr.io/cscfi/matlab:r2024a-ubuntu22.04"
