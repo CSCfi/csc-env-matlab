54748533ef7cd274fb4c7a8244507c4ef4b76d1d
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-05-03 09:45:31 +0300

    remove sudo

--- deps/matlab/r2023b/ubuntu22.04/build.sh
+++ deps/matlab/r2023b/ubuntu22.04/build.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 cd "${0%/*}"
 
 # Build the container
-sudo podman build --tag "ghcr.io/cscfi/matlab:r2023b-ubuntu22.04" .
+podman build --tag "ghcr.io/cscfi/matlab:r2023b-ubuntu22.04" .
 
 # Login to GitHub container registry
 # Prompts for an access token
-sudo podman login -u cscfi ghcr.io
+podman login -u cscfi ghcr.io
 
 # Push the container
-sudo podman push "ghcr.io/cscfi/matlab:r2023b-ubuntu22.04"
+podman push "ghcr.io/cscfi/matlab:r2023b-ubuntu22.04"
