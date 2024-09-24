fd863ae8d5b84fece7dbdcc81b6324b10d0a0b5e
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-09-24 15:50:09 +0300

    new version

---
+++ mps-puhti/r2024b/matlab.lua
@@ -0,0 +1,22 @@
+family("matlab")
+
+-- Version string
+version = myModuleVersion()
+
+-- Path to the application directory.
+appl_dir = "/appl/soft/math/matlab"
+
+-- Add wrapper scripts to the path.
+bin_dir = pathJoin(appl_dir, "mps", version, "bin")
+prepend_path("PATH", bin_dir)
+
+-- Path to the matlab container image to run using apptainer or singularity.
+image = pathJoin(appl_dir, "mps", version, "matlab.sif")
+setenv("MATLAB_SING_IMAGE", image)
+
+-- Load message and license disclaimer
+loadmsg = "MATLAB Parallel Server\nVersion: " .. version .. "\nLicense: Academic\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n"
+
+if (mode() == "load") then
+  LmodMessage(loadmsg)
+end
