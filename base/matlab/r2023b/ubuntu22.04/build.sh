716e5ad8cf7d7bc41fc73d8e59aad895d23743e4
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-09-27 14:49:15 +0300

    containers

--- base/matlab/r2023b/ubuntu22.04/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# directory of the script
-cd "${0%/*}"
-
-# Build the container
-podman build --tag "ghcr.io/cscfi/matlab:r2023b-ubuntu22.04" .
--- base/matlab/r2024b/ubuntu22.04/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# directory of the script
-cd "${0%/*}"
-
-# Build the container
-podman build --tag "ghcr.io/cscfi/matlab:r2024a-ubuntu22.04" .
