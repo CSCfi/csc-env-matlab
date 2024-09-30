8c6e29897a3453e8e5eb6eb5999ef97275d2658c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-09-30 10:32:35 +0300

    matlab proxy version as argument

--- container/R2024a/ubuntu22.04/podman.sh
+++ container/R2024a/ubuntu22.04/podman.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 podman build \
     --build-arg "MLM_LICENSE_FILE=1766@license4.csc.fi" \
+    --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
     --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_puhti.txt" \
     --tag "localhost/matlab:R2024a" \
     --file Dockerfile \
