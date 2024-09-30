e5f6566d00d6e0205056307346472946bfe3ea93
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-09-30 10:52:18 +0300

    build script

---
+++ container/r2024a/ubuntu22.04/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+build_puhti() {
+    podman build \
+        --build-arg "MLM_LICENSE_FILE=1766@license4.csc.fi" \
+        --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
+        --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_puhti.txt" \
+        --tag "localhost/matlab:r2024a-puhti" \
+        --file Dockerfile \
+        .
+}
+
+build_lumi() {
+    podman build \
+        --build-arg "MLM_LICENSE_FILE=1766@license10.csc.fi" \
+        --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
+        --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_lumi.txt" \
+        --tag "localhost/matlab:r2024a-lumi" \
+        --file Dockerfile \
+        .
+}
+
+case "$1" in
+    puhti) build_puhti ;;
+    lumi) build_lumi ;;
+    *) exit ;;
+esac
--- container/r2024a/ubuntu22.04/podman.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-podman build \
-    --build-arg "MLM_LICENSE_FILE=1766@license4.csc.fi" \
-    --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
-    --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_puhti.txt" \
-    --tag "localhost/matlab:r2024a" \
-    --file Dockerfile \
-    .
