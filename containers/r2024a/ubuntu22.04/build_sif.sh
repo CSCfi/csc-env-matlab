b7eabe6ea0b52d8f2a32c15dc29578dd244cef67
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-10-02 10:57:57 +0300

    install containerized support matlab

--- containers/r2024a/ubuntu22.04/build_sif.sh
+++ containers/r2024a/ubuntu22.04/build_sif.sh
@@ -3,6 +3,15 @@
 # Changes directory to script's location or exits on failure.
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
+build_apptainer_support() {
+    apptainer build \
+        --build-arg "MLM_LICENSE_FILE=" \
+        --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
+        --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_support.txt" \
+        matlab-r2024a-support.sif \
+        matlab.def
+}
+
 build_apptainer_puhti() {
     apptainer build \
         --build-arg "MLM_LICENSE_FILE=1766@license4.csc.fi" \
@@ -22,6 +31,7 @@ build_apptainer_lumi() {
 }
 
 case "$1" in
+    support) build_apptainer_support ;;
     puhti) build_apptainer_puhti ;;
     lumi) build_apptainer_lumi ;;
     *) exit 1 ;;
