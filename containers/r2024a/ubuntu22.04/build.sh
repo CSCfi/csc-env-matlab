e285124780c041111b1f102fbd512e9777287a06
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-10-02 09:31:34 +0300

    apptainer recipe

--- containers/r2024a/ubuntu22.04/build.sh
+++ containers/r2024a/ubuntu22.04/build.sh
@@ -3,7 +3,7 @@
 # Changes directory to script's location or exits on failure.
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
-build_puhti() {
+build_podman_puhti() {
     podman build \
         --build-arg "MLM_LICENSE_FILE=1766@license4.csc.fi" \
         --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
@@ -13,7 +13,7 @@ build_puhti() {
         .
 }
 
-build_lumi() {
+build_podman_lumi() {
     podman build \
         --build-arg "MLM_LICENSE_FILE=1766@license10.csc.fi" \
         --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
@@ -23,8 +23,26 @@ build_lumi() {
         .
 }
 
+build_apptainer_puhti() {
+    apptainer build \
+        --build-arg "MLM_LICENSE_FILE=1766@license4.csc.fi" \
+        --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
+        --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_puhti.txt" \
+        matlab-r2024a-puhti.sif \
+        matlab.def
+}
+
+build_apptainer_lumi() {
+    apptainer build \
+        --build-arg "MLM_LICENSE_FILE=1766@license10.csc.fi" \
+        --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
+        --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_lumi.txt" \
+        matlab-r2024a-lumi.sif \
+        matlab.def
+}
+
 case "$1" in
-    puhti) build_puhti ;;
-    lumi) build_lumi ;;
-    *) exit ;;
+    puhti) build_apptainer_puhti ;;
+    lumi) build_apptainer_lumi ;;
+    *) exit 1 ;;
 esac
