643fd70bc3d14e153ef0c2eace4ee475cdcc0f35
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-10-02 10:10:38 +0300

    build oci and sif

---
+++ containers/r2024a/ubuntu22.04/build_sif.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Changes directory to script's location or exits on failure.
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
+build_apptainer_puhti() {
+    apptainer build \
+        --build-arg "MLM_LICENSE_FILE=1766@license4.csc.fi" \
+        --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
+        --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_puhti.txt" \
+        matlab-r2024a-puhti.sif \
+        matlab.def
+}
+
+build_apptainer_lumi() {
+    apptainer build \
+        --build-arg "MLM_LICENSE_FILE=1766@license10.csc.fi" \
+        --build-arg "MATLAB_PROXY_VERSION=0.23.1" \
+        --build-arg "MPM_INPUT_FILE=./mpm_input_r2024a_lumi.txt" \
+        matlab-r2024a-lumi.sif \
+        matlab.def
+}
+
+case "$1" in
+    puhti) build_apptainer_puhti ;;
+    lumi) build_apptainer_lumi ;;
+    *) exit 1 ;;
+esac
