b249b5bdcfcb417993728827fa3e3f91d4bf1695
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-10-10 09:18:36 +0300

    fixes

--- containers/r2024a/ubuntu22.04/binaries.sh
+++ containers/r2024a/ubuntu22.04/binaries.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+MATLAB_CONTAINER=${1:-matlab.sif}
 mkdir -p bin bin/util bin/glnxa64
-apptainer exec --bind /run/user matlab.sif bash -c "
-cp -n /opt/matlab/bin/mw_mpiexec bin/mw_mpiexec
-cp -n /opt/matlab/bin/mw_smpd bin/mw_smpd
-cp -n /opt/matlab/bin/util/arch.sh bin/util/arch.sh
-cp -n /opt/matlab/bin/glnxa64/mpiexec bin/glnxa64/mpiexec
-cp -n /opt/matlab/bin/glnxa64/mpiexec.hydra bin/glnxa64/mpiexec.hydra
-cp -n /opt/matlab/bin/glnxa64/hydra_pmi_proxy bin/glnxa64/hydra_pmi_proxy
-cp -n /opt/matlab/bin/glnxa64/mpiexec2 bin/glnxa64/mpiexec2
-cp -n /opt/matlab/bin/glnxa64/smpd bin/glnxa64/smpd
-cp -n /opt/matlab/bin/glnxa64/libssl-mw.so.3 bin/glnxa64/libssl-mw-so.3
-cp -n /opt/matlab/bin/glnxa64/libcrypto-mw.so.3 bin/glnxa64/libcrypto-mw-so.3
+apptainer exec --bind /run/user "$MATLAB_CONTAINER" bash -c "
+cp -f /opt/matlab/bin/mw_mpiexec bin/mw_mpiexec
+cp -f /opt/matlab/bin/mw_smpd bin/mw_smpd
+cp -f /opt/matlab/bin/util/arch.sh bin/util/arch.sh
+cp -f /opt/matlab/bin/glnxa64/mpiexec bin/glnxa64/mpiexec
+cp -f /opt/matlab/bin/glnxa64/mpiexec.hydra bin/glnxa64/mpiexec.hydra
+cp -f /opt/matlab/bin/glnxa64/hydra_pmi_proxy bin/glnxa64/hydra_pmi_proxy
+cp -f /opt/matlab/bin/glnxa64/mpiexec2 bin/glnxa64/mpiexec2
+cp -f /opt/matlab/bin/glnxa64/smpd bin/glnxa64/smpd
+cp -f /opt/matlab/bin/glnxa64/libssl-mw.so.3 bin/glnxa64/libssl-mw.so.3
+cp -f /opt/matlab/bin/glnxa64/libcrypto-mw.so.3 bin/glnxa64/libcrypto-mw.so.3
 "
