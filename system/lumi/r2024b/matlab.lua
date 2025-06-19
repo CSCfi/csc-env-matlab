02ee4c995d897fdb6ee3ce902fa6087f91e9f01d
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-06-19 13:56:43 +0300

    fix

---
+++ system/lumi/r2024b/matlab.lua
@@ -0,0 +1,36 @@
+family("matlab")
+
+-- Version string
+version = myModuleVersion()
+
+-- Path to the application directory.
+appl_dir = "/appl/local/csc/soft/math"
+
+-- Add wrapper scripts to the path.
+bin_dir = pathJoin(appl_dir, "matlab", version, "bin")
+prepend_path("PATH", bin_dir)
+
+-- Path to the matlab container image run using `apptainer_wrapper`.
+image = pathJoin(appl_dir, "matlab", version, "matlab.sif")
+setenv("MATLAB_SING_IMAGE", image)
+
+-- Set license
+license_default = os.getenv("MLM_LICENSE_FILE")
+if license_default == nil or license_default == "" then
+    license = "1766@license10.csc.fi"
+elseif license_default == "/opt/matlab/licenses/network.lic" then
+    license = "1766@license10.csc.fi"
+else
+    license = license_default
+end
+setenv("MLM_LICENSE_FILE", license)
+
+-- Mathworks servicehost
+setenv("MATHWORKS_SERVICE_HOST_MANAGED_INSTALL_ROOT", pathJoin(appl_dir, "mathworksservicehost"))
+
+-- Load message and license disclaimer
+loadmsg = "MATLAB\nVersion: " .. version .. "\nLicense: Academic\n----------\nThe terms of use of this software allow its use for only in teaching and academic research at a degree-granting institute. If you are unsure whether you are allowed to use the software, please unload this module now.\n\nIf you want to use your own license with the installation, please contact LUMI support for help.\n----------"
+
+if (mode() == "load") then
+  LmodMessage(loadmsg)
+end
