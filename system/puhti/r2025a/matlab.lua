f515a540f9f5e674c6357f1a6b8eee02733be6b4
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-10-07 14:00:58 +0300

    use csc license as default for now

--- system/puhti/r2025a/matlab.lua
+++ system/puhti/r2025a/matlab.lua
@@ -10,10 +10,16 @@ appl_dir = "/appl/soft/math"
 bin_dir = pathJoin(appl_dir, "matlab", "mps", version, "bin")
 prepend_path("PATH", bin_dir)
 
--- Unset the legacy default value in OOD.
-if os.getenv("MLM_LICENSE_FILE") == "/opt/matlab/licenses/network.lic" then
-    unsetenv("MLM_LICENSE_FILE")
+-- Set license
+license_default = os.getenv("MLM_LICENSE_FILE")
+if license_default == nil or license_default == "" then
+    license = "1766@license4.csc.fi"
+elseif license_default == "/opt/matlab/licenses/network.lic" then
+    license = "1766@license4.csc.fi"
+else
+    license = license_default
 end
+setenv("MLM_LICENSE_FILE", license)
 
 -- Mathworks servicehost
 setenv("MATHWORKS_SERVICE_HOST_MANAGED_INSTALL_ROOT", pathJoin(appl_dir, "mathworksservicehost"))
