4675c08a200081e9119eca530ef7fc79e2150004
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-10-08 11:41:31 +0300

    updates

--- system/puhti/r2023b/matlab.lua
+++ system/puhti/r2023b/matlab.lua
@@ -6,28 +6,18 @@ version = myModuleVersion()
 -- Path to the application directory.
 appl_dir = "/appl/soft/math"
 
--- Add wrapper scripts to the path.
+-- Add scripts to the path.
 bin_dir = pathJoin(appl_dir, "matlab", "mps", version, "bin")
 prepend_path("PATH", bin_dir)
 
--- Path to the matlab container image to run using apptainer or singularity.
-image = pathJoin(appl_dir, "matlab", "mps", version, "matlab.sif")
-setenv("MATLAB_SING_IMAGE", image)
-
 -- Set license
-license_default = os.getenv("MLM_LICENSE_FILE")
-if license_default == nil or license_default == "" then
-    license = "1766@license4.csc.fi"
-elseif license_default == "/opt/matlab/licenses/network.lic" then
-    license = "1766@license4.csc.fi"
-else
-    license = license_default
-end
+license = os.getenv("MLM_LICENSE_FILE") or "1766@license4.csc.fi"
 setenv("MLM_LICENSE_FILE", license)
 
 -- Load message and license disclaimer
-loadmsg = "MATLAB\nVersion: " .. version .. "\nLicense: Academic\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n----------"
-
 if (mode() == "load") then
-  LmodMessage(loadmsg)
+    LmodMessage("MATLAB  " .. version .. "\n")
+    if (license == "1766@license4.csc.fi") then
+        LmodMessage("License: Academic (" .. license .. ")\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n----------")
+    end
 end
