eb995fe95e4adf5e9b6dbf68506ce3d2764a0c49
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-10-08 10:52:51 +0300

    updates

--- system/puhti/r2025a/extract_binaries.sh
+++ system/puhti/r2025a/extract_binaries.sh
@@ -2,12 +2,12 @@
 MATLAB_CONTAINER=${1:-matlab.sif}
 mkdir -p bin bin/util bin/glnxa64
 apptainer exec --bind=/run/user --bind="$PWD" "$MATLAB_CONTAINER" bash -c "
-cp -f /opt/matlab/bin/mw_mpiexec bin/mw_mpiexec
-cp -f /opt/matlab/bin/util/arch.sh bin/util/arch.sh
-cp -f /opt/matlab/bin/glnxa64/mpiexec bin/glnxa64/mpiexec
-cp -f /opt/matlab/bin/glnxa64/mpiexec.hydra bin/glnxa64/mpiexec.hydra
-cp -f /opt/matlab/bin/glnxa64/hydra_pmi_proxy bin/glnxa64/hydra_pmi_proxy
-cp -f /opt/matlab/bin/glnxa64/libssl-mw.so.3 bin/glnxa64/libssl-mw.so.3
-cp -f /opt/matlab/bin/glnxa64/libcrypto-mw.so.3 bin/glnxa64/libcrypto-mw.so.3
-cp -rf /opt/matlab/bin/glnca64/impi bin/glnxa64/impi
+cp -f /opt/matlab/bin/mw_mpiexec $PWD/bin/mw_mpiexec
+cp -f /opt/matlab/bin/util/arch.sh $PWD/bin/util/arch.sh
+cp -f /opt/matlab/bin/glnxa64/mpiexec $PWD/bin/glnxa64/mpiexec
+cp -f /opt/matlab/bin/glnxa64/mpiexec.hydra $PWD/bin/glnxa64/mpiexec.hydra
+cp -f /opt/matlab/bin/glnxa64/hydra_pmi_proxy $PWD/bin/glnxa64/hydra_pmi_proxy
+cp -f /opt/matlab/bin/glnxa64/libssl-mw.so.3 $PWD/bin/glnxa64/libssl-mw.so.3
+cp -f /opt/matlab/bin/glnxa64/libcrypto-mw.so.3 $PWD/bin/glnxa64/libcrypto-mw.so.3
+cp -rf /opt/matlab/bin/glnca64/impi $PWD/bin/glnxa64/impi
 "
--- system/puhti/r2025a/matlab.lua
+++ system/puhti/r2025a/matlab.lua
@@ -6,7 +6,7 @@ version = myModuleVersion()
 -- Path to the application directory.
 appl_dir = "/appl/soft/math"
 
--- Add wrapper scripts to the path.
+-- Add scripts to the path.
 bin_dir = pathJoin(appl_dir, "matlab", "mps", version, "bin")
 prepend_path("PATH", bin_dir)
 
@@ -15,6 +15,9 @@ license = os.getenv("MLM_LICENSE_FILE") or "1766@license4.csc.fi"
 setenv("MLM_LICENSE_FILE", license)
 
 -- Load message and license disclaimer
-if (mode() == "load" and license == "1766@license4.csc.fi") then
-    LmodMessage("MATLAB  " .. version .. "\nLicense: Academic (" .. license .. ")\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n----------")
+if (mode() == "load") then
+    LmodMessage("MATLAB  " .. version .. "\n")
+    if (license == "1766@license4.csc.fi") then
+        LmodMessage("License: Academic (" .. license .. ")\n----------\nThe terms of use of this software allow its use for only the affiliates (staff and students) of Finnish higher education institutions. If you are unsure whether you are allowed to use the software, please unload this module now.\n----------")
+    end
 end
