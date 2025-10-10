b587cb76bdbed40954bd7da1d1949ad5fdf3f829
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-10-10 14:38:45 +0300

    fix typo

--- system/puhti/r2025a/extract_binaries.sh
+++ system/puhti/r2025a/extract_binaries.sh
@@ -9,5 +9,5 @@ cp -f /opt/matlab/bin/glnxa64/mpiexec.hydra $PWD/bin/glnxa64/mpiexec.hydra
 cp -f /opt/matlab/bin/glnxa64/hydra_pmi_proxy $PWD/bin/glnxa64/hydra_pmi_proxy
 cp -f /opt/matlab/bin/glnxa64/libssl-mw.so.3 $PWD/bin/glnxa64/libssl-mw.so.3
 cp -f /opt/matlab/bin/glnxa64/libcrypto-mw.so.3 $PWD/bin/glnxa64/libcrypto-mw.so.3
-cp -rf /opt/matlab/bin/glnca64/impi $PWD/bin/glnxa64/impi
+cp -rf /opt/matlab/bin/glnxa64/impi $PWD/bin/glnxa64/impi
 "
