92d368ad930c8e52526028699ec8a224581d8cfb
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-10-21 15:24:48 +0300

    gpu test

---
+++ test/mps-puhti/funcGPU.m
@@ -0,0 +1,7 @@
+function C = funcGPU(n)
+gpuDevice
+A_gpu = gpuArray(rand(n, n));
+B_gpu = gpuArray(rand(n, n));
+C_gpu = A_gpu * B_gpu;
+C = gather(C_gpu);
+end
--- test/mps-puhti/test_gpu.m
+++ test/mps-puhti/test_gpu.m
@@ -7,5 +7,5 @@ c.NumThreads = 4;                                      % --cpus-per-task=<NumThr
 c.AdditionalProperties.MemPerCPU = '1g';                % --mem-per-cpu=<MemPerCPU>
 c.AdditionalProperties.GPUCard = 'v100';                % --gres=gpu:<GPUCard>:<GPUsPerNode>
 c.AdditionalProperties.GPUsPerNode = 1;
-j = batch(c, @gpuDevice, 1, {}, 'CurrentFolder', '.', 'AutoAddClientPath',false);
+j = batch(c, 'funcGPU', 1, {1000}, 'CurrentFolder', '.', 'AutoAddClientPath',false);
 end
