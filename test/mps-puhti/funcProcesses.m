f3b3325adfa18f18e7921d32875dc9695ec7656c
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2025-10-22 10:11:25 +0300

    processes

--- test/mps-puhti/funcProcesses.m
+++ test/mps-puhti/funcProcesses.m
@@ -1,5 +1,7 @@
 function t = funcProcesses(n)
 pool = parpool('Processes', n);
+disp(funcParallel(n));
 t = funcParallel(n);
+disp(t);
 delete(pool);
 end
---
+++ test/mps-puhti/test_small_processes.m
@@ -0,0 +1,12 @@
+function j = test_small_processes()
+n = 2;
+c = parcluster();
+c.AdditionalProperties.ComputingProject = 'project_2001659';  % --account=<ComputingProject>
+c.AdditionalProperties.Partition = 'test';             % --partition=<Partition>
+c.AdditionalProperties.WallTime = '00:15:00';           % --time=<WallTime>
+c.NumThreads = n;                                       % --cpus-per-task=<NumThreads>
+c.AdditionalProperties.MemPerCPU = '2g';                % --mem-per-cpu=<MemPerCPU>
+c.AdditionalProperties.GPUCard = '';                    % --gres=gpu:<GPUCard>:<GPUsPerNode>
+c.AdditionalProperties.GPUsPerNode = 0;
+j = batch(c, 'funcProcesses', 1, {n}, 'CurrentFolder', '.', 'AutoAddClientPath', false);
+end
