2cefe322e4f2f0f40caa4a03f0fc83c7d054c405
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-03-04 16:07:17 +0200

    extract binaries

---
+++ system/lumi/version/r2023b/extract_binaries.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+MATLAB_CONTAINER=${1:-matlab.sif}
+mkdir -p bin bin/util bin/glnxa64
+singularity exec --bind=/run/user --bind="$PWD" "$MATLAB_CONTAINER" bash -c "
+cp -f /opt/matlab/bin/mw_mpiexec $PWD/bin/mw_mpiexec
+cp -f /opt/matlab/bin/mw_smpd $PWD/bin/mw_smpd
+cp -f /opt/matlab/bin/util/arch.sh $PWD/bin/util/arch.sh
+cp -f /opt/matlab/bin/glnxa64/mpiexec $PWD/bin/glnxa64/mpiexec
+cp -f /opt/matlab/bin/glnxa64/mpiexec.hydra $PWD/bin/glnxa64/mpiexec.hydra
+cp -f /opt/matlab/bin/glnxa64/hydra_pmi_proxy $PWD/bin/glnxa64/hydra_pmi_proxy
+cp -f /opt/matlab/bin/glnxa64/mpiexec2 $PWD/bin/glnxa64/mpiexec2
+cp -f /opt/matlab/bin/glnxa64/smpd $PWD/bin/glnxa64/smpd
+cp -f /opt/matlab/bin/glnxa64/libssl-mw.so.3 $PWD/bin/glnxa64/libssl-mw.so.3
+cp -f /opt/matlab/bin/glnxa64/libcrypto-mw.so.3 $PWD/bin/glnxa64/libcrypto-mw.so.3
+"
---
+++ system/lumi/version/r2025a/extract_binaries.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+MATLAB_CONTAINER=${1:-matlab.sif}
+mkdir -p bin bin/util bin/glnxa64
+singularity exec --bind=/run/user --bind="$PWD" "$MATLAB_CONTAINER" bash -c "
+cp -f /opt/matlab/bin/mw_mpiexec $PWD/bin/mw_mpiexec
+cp -f /opt/matlab/bin/util/arch.sh $PWD/bin/util/arch.sh
+cp -f /opt/matlab/bin/glnxa64/mpiexec $PWD/bin/glnxa64/mpiexec
+cp -f /opt/matlab/bin/glnxa64/mpiexec.hydra $PWD/bin/glnxa64/mpiexec.hydra
+cp -f /opt/matlab/bin/glnxa64/hydra_pmi_proxy $PWD/bin/glnxa64/hydra_pmi_proxy
+cp -f /opt/matlab/bin/glnxa64/libssl-mw.so.3 $PWD/bin/glnxa64/libssl-mw.so.3
+cp -f /opt/matlab/bin/glnxa64/libcrypto-mw.so.3 $PWD/bin/glnxa64/libcrypto-mw.so.3
+cp -rf /opt/matlab/bin/glnxa64/impi $PWD/bin/glnxa64/impi
+"
